ae1f78295b38db04aa3e1c8146ed0add5ff4272e
tinus100 <70579080+tinus100@users.noreply.github.com>
2024-02-05 10:33:08 +0100

    Update UserFactory.php

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace Database\Factories;
 
 use App\Models\User;
@@ -8,22 +6,13 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'voornaam' => $this->faker->firstName, // Gebruik firstName voor de voornaam
+            'achternaam' => $this->faker->lastName, // Gebruik lastName voor de achternaam
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -31,11 +20,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
     public function unverified()
     {
         return $this->state(function (array $attributes) {
@@ -45,3 +29,4 @@ class UserFactory extends Factory
         });
     }
 }
+
