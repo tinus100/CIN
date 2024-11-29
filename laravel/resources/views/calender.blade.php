816c76b8f3f47b97dc47b92a5209a9dc18f49497
tinus100 <tijn.timmik@hotmail.com>
2024-11-29 10:17:58 +0100

    testen

--- laravel/resources/views/calender.blade.php
+++ laravel/resources/views/calender.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kledingbank Zeeland agenda!</title>
+    <title>Kledingbank Zeeland agendaa</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
